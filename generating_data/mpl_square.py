1ef17c215d54463bcbb11c61bb743f56160910d7
PrabinKayastha <princekayastha@gmail.com>
2020-01-28 14:41:28 +0545

    added labels to the graph.

--- generating_data/mpl_square.py
+++ generating_data/mpl_square.py
@@ -5,6 +5,14 @@ import matplotlib.pyplot as plt
 squares = [1, 4, 9, 16, 25]
 
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.plot(squares, linewidth = 3)
+
+# Set the title chart and label axis.
+ax.set_title("Square Numbers", fontsize = 25)
+ax.set_xlabel("Value", fontsize  =15)
+ax.set_ylabel("Square of Value", fontsize  =15)
 
 plt.show()
+
+
+# %%
