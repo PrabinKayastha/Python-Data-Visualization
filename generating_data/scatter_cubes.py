50b3bc33872050f15c337e46b24d1d80941be9ca
Prabin Kayastha <princekayastha@gmail.com>
2020-01-28 19:11:50 +0545

    cube scatterplot, needs to be fixed

---
+++ generating_data/scatter_cubes.py
@@ -0,0 +1,20 @@
+#%%
+import matplotlib.pyplot as plt
+
+plt.style.use('threefiftyeight')
+
+x_val = [x for x in range(1000)]
+y_val = [x**3 for x in x_val]
+
+fig, ax = plt.subplots()
+ax.scatter(x_val, y_val, c=y_val, cmap=plt.cm.Blues, s=15)
+
+ax.set_title('Cube scatterplots', fontsize = 25)
+ax.set_xlabel('x_val', fontsize = 15)
+ax.set_ylabel('y_vals = x_vals cubed', fontsize = 15)
+
+ax.tick_params(axis='both', which = 'major', labelsize = 15)
+
+plt.show()
+
+# %%
