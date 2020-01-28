3e04cd10de9157d46fe35ace76eb2547bed5b82e
Prabin Kayastha <princekayastha@gmail.com>
2020-01-28 17:19:16 +0545

    made changes so as to suport different values in x and y axws

--- generating_data/scatter_squares.py
+++ generating_data/scatter_squares.py
@@ -3,8 +3,13 @@ import matplotlib.pyplot as plt
 
 plt.style.use('fivethirtyeight')
 
+
+x_values = [1,2,3,4,5]
+y_values = [1,4,9,16,25]
+
+
 fig, ax = plt.subplots()
-ax.scatter(2, 4)
+ax.scatter(x_values, y_values, s=150)
 
 # Set the title chart and label axis.
 ax.set_title('Squares scatter plot.', fontsize = 25)
@@ -13,7 +18,7 @@ ax.set_ylabel("Square of Value", fontsize  =15)
 
 
 # Change the sixe of ticks in label.
-ax.tick_params(axis='both', labelsize=15)
+ax.tick_params(axis='both', which = 'major',labelsize=15)
 
 
 plt.show()
