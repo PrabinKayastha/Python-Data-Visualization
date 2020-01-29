5349cc8214120d5c6f72d386ae90f5a7d0d24743
Prabin Kayastha <princekayastha@gmail.com>
2020-01-29 14:01:27 +0545

    making the random walks until told otherwise

--- generating_data/rw_visual.py
+++ generating_data/rw_visual.py
@@ -3,12 +3,26 @@ from matplotlib import pyplot as plt
 
 from random_walk import RandomWalk
 
+# Generate the random walks as long as the program is active
+while True:
+    # Make a random walk.
+    rw = RandomWalk()
+    rw.fill_walk()
 
-# Make a random walk.
-rw = RandomWalk()
-rw.fill_walk()
+    # Plot the points in a walk.
+    plt.style.use('classic')
+    fig, ax = plt.subplots()
+    ax.scatter(rw.x_values, rw.y_values, s = 2)
 
-# Plot the points in a walk.
-plt.style.use('classic')
-fig, ax = plt.subplots()
-ax.scatter(rw.x_values, rw.y_values, s = 2)
+    keep_running = input('Make another walk? (y/n) :')
+    if keep_running.lower() == 'y':
+        print('Generating new random walk.')
+        continue
+    else:
+        if keep_running.lower() == 'n':
+            print('Exiting the walk.')
+        break
+
+
+
+# %%
