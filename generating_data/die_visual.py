e47740a47c4a5e46c78d7ef82b2bedbdd17840c5
Prabin Kayastha <princekayastha@gmail.com>
2020-01-29 17:44:52 +0545

    made changes so as to display frequencies of 2 die roll

--- generating_data/die_visual.py
+++ generating_data/die_visual.py
@@ -4,28 +4,31 @@ from plotly import offline
 from die import Die
 
 
-# Create 6 faced die.
-die = Die()
+# Create TWO 6 faced die.
+die_1 = Die()
+die_2 = Die()
+
 
 # Store the results of 100 rolls.
 results = []
 for roll_num in range(1000):
-    result = die.roll()
+    result = die_1.roll() + die_2.roll()
     results.append(result)
 
 # Analyze the result.
 frequencies = []
-for value in range(1, die.num_sides + 1):
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(2, max_result + 1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
 # Visualize the results.
-x_val = list(range(1, die.num_sides + 1))
+x_val = list(range(2, max_result + 1))
 data = [Bar(x=x_val, y=frequencies)]
 
-x_axis_config = {'title':'Result'}
+x_axis_config = {'title':'Result', 'dtick':1}
 y_axis_config = {'title':'Frequency of Result'}
-my_layout = Layout(title='Results of rolling one D6 1000 times.', xaxis = x_axis_config, yaxis=y_axis_config)
-offline.plot({'data':data, 'layout':my_layout}, filename='d6.html')
+my_layout = Layout(title='Results of rolling two D6 1000 times.', xaxis = x_axis_config, yaxis=y_axis_config)
+offline.plot({'data':data, 'layout':my_layout}, filename='d6_d6.html')
 
 print(frequencies)
