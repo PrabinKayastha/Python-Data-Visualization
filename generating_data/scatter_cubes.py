0302dbc2b374412c9121ce621fd879b8b4168cd6
Prabin Kayastha <princekayastha@gmail.com>
2020-01-29 11:30:38 +0545

    changed the cmap color and corrected the style for the scatter plot

--- generating_data/scatter_cubes.py
+++ generating_data/scatter_cubes.py
@@ -1,13 +1,13 @@
 #%%
 import matplotlib.pyplot as plt
 
-plt.style.use('threefiftyeight')
+plt.style.use('fivethirtyeight')
 
 x_val = [x for x in range(1000)]
 y_val = [x**3 for x in x_val]
 
 fig, ax = plt.subplots()
-ax.scatter(x_val, y_val, c=y_val, cmap=plt.cm.Blues, s=15)
+ax.scatter(x_val, y_val, c=y_val, cmap=plt.cm.Greys, s=15)
 
 ax.set_title('Cube scatterplots', fontsize = 25)
 ax.set_xlabel('x_val', fontsize = 15)
