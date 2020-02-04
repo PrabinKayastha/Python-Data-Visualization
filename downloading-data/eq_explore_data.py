c7b3b69a64d869f32fa6196588f566cc740ae6e7
PrabinKayastha <princekayastha@gmail.com>
2020-02-04 15:40:49 +0545

    made changes to add hover texts

--- downloading-data/eq_explore_data.py
+++ downloading-data/eq_explore_data.py
@@ -11,17 +11,19 @@ with open(filename, 'r') as f:
 
 all_eq_dicts = all_eq_data['features']
 
-mags, lons, lats = [], [], []
+mags, lons, lats, hover_texts = [], [], [], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
+    hover_text = eq_dict['properties']['title']
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+    hover_texts.append(hover_text)
 
 # Map the earthquakes.
-data = [Scattergeo(lon=lons, lat=lats, marker= {'size' : [5*mag for mag in mags], 'color':mags, 'colorscale':'Viridis', 'reversescale':True, 'colorbar':{'title':'Magnitude'}})]
+data = [Scattergeo(lon=lons, lat=lats, text=hover_texts, marker= {'size' : [5*mag for mag in mags], 'color':mags, 'colorscale':'Viridis', 'reversescale':True, 'colorbar':{'title':'Magnitude'}})]
 my_layout = Layout(title='Global Earthquakes')
 
 fig = {'data' : data, 'layout' : my_layout}
