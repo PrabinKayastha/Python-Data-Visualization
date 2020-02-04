057c3e42fe2778b19fd563d2a1be69c828e32686
Prabin Kayastha <princekayastha@gmail.com>
2020-02-04 13:54:41 +0545

    pulled the geo location for the earthquakes

--- downloading-data/eq_explore_data.py
+++ downloading-data/eq_explore_data.py
@@ -8,4 +8,14 @@ with open(filename, 'r') as f:
     all_eq_data = json.load(f)
 
 all_eq_dicts = all_eq_data['features']
-print(len(all_eq_dicts))
+
+mags, lons, lats = [], [], []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+
+print(list(zip(mags, lons, lats)))
