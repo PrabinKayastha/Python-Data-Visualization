0d19447dd47951730a97a290cf93fbf298954450
PrabinKayastha <princekayastha@gmail.com>
2020-02-05 17:48:06 +0545

    added the links to the repositories in the x-label

--- working-with-apis/python_repos_visual.py
+++ working-with-apis/python_repos_visual.py
@@ -13,18 +13,25 @@ print(f'Status Code : {r.status_code}')
 # Process results.
 response_dict = r.json()
 repo_dicts = response_dict['items']
-repo_names, stars, labels = [], [], []
+repo_names, stars, labels, repo_links = [], [], [], []
 for repo_dict in repo_dicts:
-    repo_names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
+    repo_name = repo_dict['name']
+    repo_names.append(repo_name)
+    star = repo_dict['stargazers_count']
+    stars.append(star)
 
     owner = repo_dict['owner']['login']
     description = repo_dict['description']
     label = f'{owner}</br>{description}'
     labels.append(label)
 
+    repo_url = repo_dict['html_url']
+    repo_link = f'<a href = "{repo_url}">{repo_name}</a>'
+    repo_links.append(repo_link)
+
+
 # Make visualization.
-data = [{'type':'bar', 'x' : repo_names, 'y' : stars, 'hovertext' : labels, 'marker' : {'color':'rgb(60, 100, 150)', 'line' : {'width':1.5, 'color':'rgb(25, 25, 25)'}}, 'opacity':0.6}]
+data = [{'type':'bar', 'x' : repo_links, 'y' : stars, 'hovertext' : labels, 'marker' : {'color':'rgb(60, 100, 150)', 'line' : {'width':1.5, 'color':'rgb(25, 25, 25)'}}, 'opacity':0.6}]
 
 my_layout = {
     'title' : 'Most starred Python projects on Github',
